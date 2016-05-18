83787b43675329c42e8861daa0024c2e3753cad9
romanm777 <roman1918@gmail.com>
2016-05-18 13:38:10 +0300

    All functionality has created

--- js/base.js
+++ js/base.js
@@ -3,7 +3,7 @@ var page = {
   	'@class': 'red',
   	'h3': 'welcome',
     'b': 'bold text'
-  }
+  },
   'b': 'one more bold text',
   'b': {
   	'@class': 'red',
@@ -20,13 +20,15 @@ var page = {
   }
 };
 
+"use strict"
+
 // gets all buttons on the page and find our
 var buttons = document.getElementsByTagName("button");
 for(var i = 0, count = buttons.length; i < count; ++i) {
 	var button = buttons[i];
 
   // if proper button has found
-	if(button.innerText === "Refresh") {
+	if(button.innerText === "Build") {
   	// sets the onclick handler
   	button.onclick = build;
   }
@@ -43,14 +45,60 @@ function build() {
     pageDiv.removeChild(pageDiv.firstChild);
   }
 
-  // loops through all properties of 'page' object
- 	for(var prop in page) {
-  	// creates tag and tag text
-		var newTag = document.createElement(prop);
-  	var textNode = document.createTextNode(page[prop]);
+	// recursion tree building
+	buildTree(page, pageDiv);
+}
 
-  	// appends items to page div
-  	newTag.appendChild(textNode);
-  	pageDiv.appendChild(newTag);
+function buildTree(parentObject, parentNode) {
+	// checks if parentObject is substring
+	if("string" == typeof parentObject) {
+		createValue(parentObject, parentNode);
+		return;
 	}
+
+	// loops through all properties of 'parentObject'
+ 	for(var prop in parentObject) {
+		var variant = -1;
+		var atSignIndex = prop.indexOf("@");
+
+		// creates tag value
+		if(0 == atSignIndex && prop.substring(atSignIndex + 1) == "value") {
+			createValue(parentObject[prop], parentNode);
+		}
+		// creates attribute
+		else if(0 == atSignIndex) {
+			createAttribute(prop, parentObject[prop], parentNode);
+		}
+		// creates tag
+		else {
+			createTag(prop, parentObject[prop], parentNode);
+		}
+	}
+}
+
+function createAttribute(attr, attrValue, parentNode) {
+	// cuts off "@"
+	var temp = attr;
+	var index = temp.indexOf("@");
+	if(-1 != index) {
+		temp = temp.substring(index + 1);
+	}
+
+	var attr = document.createAttribute(temp);      // Creates an attribute
+	attr.value = attrValue;                       	// Set the value of the attribute
+	parentNode.setAttributeNode(attr);
+}
+
+function createValue(tagValue, parentNode) {
+	var text = document.createTextNode(tagValue);       // Create a text node
+	parentNode.appendChild(text);
+}
+
+function createTag(prop, propValue, parentNode) {
+	// creates tag and tag text
+	var newTag = document.createElement(prop);
+	parentNode.appendChild(newTag);
+
+	// recursion
+	buildTree(propValue, newTag);
 }
